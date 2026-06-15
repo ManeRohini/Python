96beda49c6876d657a96961cd81145ecbaf914c4
ManeRohini <89936696+ManeRohini@users.noreply.github.com>
2026-06-15 23:02:42 +0530

    Delete Assignment 2 directory

--- Assignment 2/q4.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-print(sys.getsizeof(10))        # int → ~28 bytes
-print(sys.getsizeof("hello"))   # str → ~46 bytes
-print(sys.getsizeof([1]))   # list → ~64 bytes
-print(sys.getsizeof([1,2,3]))   # list → ~88 bytes
-
-#getsizeof() is a function from Python’s sys module.
-#It returns the memory size (in bytes) that a Python object occupies.
--- Assignment 2/q8.py
+++
@@ -1,6 +0,0 @@
-x=input("enter number: ")
-print(type(x))
-
-#Explain
-#the input function always returns string data in python, so the type of x is str
-#python take raw text using input() function
