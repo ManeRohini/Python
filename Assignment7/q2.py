7ff05ca631202b7d3edc2d85215786280f7f7ac4
ManeRohini <89936696+ManeRohini@users.noreply.github.com>
2026-06-27 14:29:49 +0530

    Delete Assignment7 directory

--- Assignment7/q2.py
+++
@@ -1,8 +0,0 @@
-#Why should excessive use of global variables be avoided in large programs?
-
-
-#global varible is the one declared out of scope of any function
-#it has the shared access among function inside or outside 
-#any function or conditional blockes can use this variable and can update as per there logic
-# modification of this variable may lead to logiacl error for some another variable 
-# that is the reason global variable be avoided in large programs
--- Assignment7/q7.py
+++
@@ -1,9 +0,0 @@
-
-
-#What is a module in Python? How is it different from a function?
-
-#Module in python is a set of code having function,classes,variable, logical operactions etc
-#modules can be included inside other python code file, it gives better readability and it is easy to maintain
-
-#function is a block of code inside a program which performs a specific task
-
