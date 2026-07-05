0d631cacecaa6a35844ef599b69b043d87608fc2
ManeRohini <89936696+ManeRohini@users.noreply.github.com>
2026-07-06 00:06:25 +0530

    Update q10.py

--- Assignments/Assignment15/q10.py
+++ Assignments/Assignment15/q10.py
@@ -1,2 +1,18 @@
 #10.Write a lambda function using filter() which accepts a list of numbers 
 # and returns the count of even numbers.
+
+
+filter_divisible = lambda n: n%2==0 
+
+def main():
+    Data=[13,12,8,10,11,20]
+    print("Input data is :",Data)
+
+    print("Data before  ",Data)
+
+    FData=list(filter(filter_divisible,Data))
+    
+    print("Data after  ",FData)
+
+if __name__ =="__main__":
+    main()
